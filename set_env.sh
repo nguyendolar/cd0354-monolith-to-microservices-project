287d53407d4cecaf8717654a761bc00dd8e60252
nguyendolar <65477330+nguyendolar@users.noreply.github.com>
2023-12-03 16:55:13 +0700

    Add files via upload

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=root
+export POSTGRES_PASSWORD=12345678
+export POSTGRES_HOST=cdr.cyzcalovwmrd.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::my-116840594891-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=dangdhh
+export URL=http://localhost:8100
